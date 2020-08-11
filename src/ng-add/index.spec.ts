dabfeea89484aaef119156ad55524dd1506230ae
Chau Tran <ctch5@mail.umsl.edu>
2020-08-11 11:55:40 -0500

    ready to publish

---
+++ src/ng-add/index.spec.ts
@@ -0,0 +1,93 @@
+import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
+import * as path from 'path';
+import { Schema } from './schema';
+
+const collectionPath = path.join(__dirname, '../collection.json');
+
+const workspaceOptions = {
+  name: 'workspace',
+  newProjectRoot: 'projects',
+  version: '6.0.0',
+  defaultProject: 'bar',
+};
+
+const defaultAppOptions = {
+  name: 'bar',
+  inlineStyle: false,
+  inlineTemplate: false,
+  viewEncapsulation: 'Emulated',
+  routing: false,
+  style: 'css',
+  skipTests: false,
+};
+
+
+const defaultPackages = ['@angular-builders/custom-webpack', 'postcss-import', 'postcss-loader', 'tailwindcss'];
+const purgePackage = '@fullhuman/postcss-purgecss';
+
+describe('ng add function', () => {
+  let appTree: UnitTestTree;
+  let schematicRunner: SchematicTestRunner;
+  let options: Schema;
+
+  beforeEach(async () => {
+    options = {
+      cssFlavor: 'css',
+      project: 'bar',
+      usePurgeCss: true,
+      configDirectory: '.',
+      tailwindConfigFileName: 'tailwind.config'
+    };
+    schematicRunner = new SchematicTestRunner('tailwind-schematics', collectionPath);
+    appTree = await schematicRunner
+      .runExternalSchematicAsync(
+        '@schematics/angular',
+        'workspace',
+        workspaceOptions
+      )
+      .toPromise();
+
+    appTree = await schematicRunner
+      .runExternalSchematicAsync(
+        '@schematics/angular',
+        'application',
+        defaultAppOptions,
+        appTree
+      )
+      .toPromise();
+  });
+
+  function assertDefaultPackages(packageJson: string) {
+    defaultPackages.forEach(pkg => {
+      expect(packageJson).toContain(pkg);
+    });
+  }
+
+  it('should add proper packages to devDependencies with default options', async () => {
+    const tree = await schematicRunner.runSchematicAsync('ng-add', options, appTree).toPromise();
+    const packageJson = tree.readContent('/package.json');
+    expect(packageJson).toBeTruthy();
+    assertDefaultPackages(packageJson);
+    expect(packageJson).toContain(purgePackage);
+  });
+
+  it('should add proper packages to devDependencies without purgeCSS', async () => {
+    options.usePurgeCss = false;
+    const tree = await schematicRunner.runSchematicAsync('ng-add', options, appTree).toPromise();
+    const packageJson = tree.readContent('/package.json');
+    expect(packageJson).toBeTruthy();
+    assertDefaultPackages(packageJson);
+    expect(packageJson).not.toContain(purgePackage);
+  });
+
+  it('should add proper packages to devDependencies with each cssFlavor', async () => {
+    for (let flavor of ['scss', 'less', 'sass', 'styl']) {
+      options.cssFlavor = flavor;
+      const tree = await schematicRunner.runSchematicAsync('ng-add', options, appTree).toPromise();
+      const packageJson = tree.readContent('/package.json');
+      expect(packageJson).toBeTruthy();
+      assertDefaultPackages(packageJson);
+      expect(packageJson).toContain(`postcss-${ flavor }`);
+    }
+  });
+});
--- src/ng-add/setup-project.spec.ts
+++ src/ng-add/setup-project.spec.ts
@@ -1,1 +1,114 @@
+import { PathFragment } from '@angular-devkit/core';
+import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
+import * as path from 'path';
+import { Schema } from './schema';
 
+const collectionPath = path.join(__dirname, '../collection.json');
+
+const workspaceOptions = {
+  name: 'workspace',
+  newProjectRoot: 'projects',
+  version: '6.0.0',
+  defaultProject: 'bar',
+};
+
+const defaultAppOptions = {
+  name: 'bar',
+  inlineStyle: false,
+  inlineTemplate: false,
+  viewEncapsulation: 'Emulated',
+  routing: false,
+  style: 'css',
+  skipTests: false,
+};
+
+describe('setup-project', () => {
+  let appTree: UnitTestTree;
+  let schematicRunner: SchematicTestRunner;
+  let options: Schema;
+
+  beforeEach(async () => {
+    options = {
+      cssFlavor: 'css',
+      project: 'bar',
+      usePurgeCss: true,
+      configDirectory: '.',
+      tailwindConfigFileName: 'tailwind.config'
+    };
+    schematicRunner = new SchematicTestRunner('tailwind-schematics', collectionPath);
+    appTree = await schematicRunner
+      .runExternalSchematicAsync(
+        '@schematics/angular',
+        'workspace',
+        workspaceOptions
+      )
+      .toPromise();
+
+    appTree = await schematicRunner
+      .runExternalSchematicAsync(
+        '@schematics/angular',
+        'application',
+        defaultAppOptions,
+        appTree
+      )
+      .toPromise();
+  });
+
+  it('should create proper config files', async () => {
+    const tree = await schematicRunner.runSchematicAsync('ng-add-setup-project', options, appTree).toPromise();
+    const configDir = tree.getDir(options.configDirectory);
+    expect(configDir).toBeTruthy();
+    const configFiles = configDir.subfiles;
+    [
+      `${ options.tailwindConfigFileName }.js`,
+      'webpack-dev.config.js',
+      'webpack-prod.config.js'
+    ].forEach(configFile => {
+      expect(configFiles.includes(configFile as PathFragment)).toBeTrue();
+    });
+  });
+
+  it('should create proper config files without purgeCSS', async () => {
+    options.usePurgeCss = false;
+    const tree = await schematicRunner.runSchematicAsync('ng-add-setup-project', options, appTree).toPromise();
+    const configDir = tree.getDir(options.configDirectory);
+    expect(configDir).toBeTruthy();
+    const configFiles = configDir.subfiles;
+    [
+      `${ options.tailwindConfigFileName }.js`,
+      'webpack.config.js'
+    ].forEach(configFile => {
+      expect(configFiles.includes(configFile as PathFragment)).toBeTrue();
+    });
+  });
+
+  it('should use custom webpack in angular.json', async () => {
+    const tree = await schematicRunner.runSchematicAsync('ng-add-setup-project', options, appTree).toPromise();
+    const angularJson = tree.readContent('/angular.json');
+    expect(angularJson).toContain('customWebpackConfig');
+    const configPath = options.configDirectory === '.' ? '' : (options.configDirectory.substring(2) + '/');
+    expect(angularJson).toContain(`${ configPath }webpack-dev.config.js`);
+    expect(angularJson).toContain(`${ configPath }webpack-prod.config.js`);
+  });
+
+  it('should use custom webpack in angular.json without purgeCSS', async () => {
+    options.usePurgeCss = false;
+    const tree = await schematicRunner.runSchematicAsync('ng-add-setup-project', options, appTree).toPromise();
+    const angularJson = tree.readContent('/angular.json');
+    expect(angularJson).toContain('customWebpackConfig');
+    const configPath = options.configDirectory === '.' ? '' : (options.configDirectory.substring(2) + '/');
+    expect(angularJson).toContain(`${ configPath }webpack.config.js`);
+  });
+
+  it('should include Tailwind imports in main styles', async () => {
+    const tree = await schematicRunner.runSchematicAsync('ng-add-setup-project', options, appTree).toPromise();
+    const styleContent = tree.readContent('/projects/bar/src/styles.css');
+    [
+      '@import \'tailwindcss/base\'',
+      '@import \'tailwindcss/components\'',
+      '@import \'tailwindcss/utilities\'',
+    ].forEach(i => {
+      expect(styleContent).toContain(i);
+    });
+  });
+});
