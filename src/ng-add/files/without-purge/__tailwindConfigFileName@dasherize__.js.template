0edfb91ce48725f36ee9b19c9834de9cf610eb9f
Chau Tran <ctch5@mail.umsl.edu>
2020-08-11 00:36:12 -0500

    before changed

---
+++ src/ng-add/files/without-purge/__tailwindConfigFileName@dasherize__.js.template
@@ -0,0 +1,5 @@
+module.exports = {
+  prefix: '',
+  separator: ':',
+  theme: {}
+}
---
+++ src/ng-add/index.ts
@@ -0,0 +1,7 @@
+import { Rule } from '@angular-devkit/schematics';
+
+export default function (): Rule {
+  return (tree, _context) => {
+    return tree;
+  };
+}
